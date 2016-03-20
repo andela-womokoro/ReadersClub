d439ca529cbc40827d966fdf4272804a41e8f0f0
Wilson Omokoro <wilson.omokoro@andela.com>
2016-03-20 17:44:02 +0100

    Add reg date property to Members class

--- src/readersClub/Members.java
+++ src/readersClub/Members.java
@@ -1,6 +1,7 @@
 
 package readersClub;
 
+import java.util.Date;
 /**
  *
  * @author Wilson Omokoro
@@ -10,12 +11,14 @@ public abstract class Members {
     public String name;
     public String sex;
     public int age;
+    public Date regDate;
     
     public Members(String name, String sex, int age){
        System.out.println("Creating a new member...");
        this.name = name;
        this.sex = sex;
        this.age = age;
+       this.regDate = new Date();
     }
     
     public String getName(){
