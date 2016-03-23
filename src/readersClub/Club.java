51ab25fa8a920b90996b3ea866ed4ce587600bd6
Wilson Omokoro <wilson.omokoro@andela.com>
2016-03-23 13:36:24 +0100

    Implement createBook and createMember methods in Club class

--- src/readersClub/Club.java
+++ src/readersClub/Club.java
@@ -1,6 +1,8 @@
 
 package readersClub;
 
+import java.util.Objects;
+
 /**
  *
  * @author Wilson Omokoro
@@ -14,20 +16,32 @@ public class Club {
     public static void main(String[] args){
         
         //create some books
-        Book book1 = new Book("Alice in Wonderland","3772819", "Lewis Carol");
+        createBook("Alice in Wonderland", "3772819", "Lewis Carol");
+        createBook("Frankinstein", "06672679", "Mary Shelley");
+        createBook("Spectre", "7665923", "Ian Fleming");
+        createBook("Havoc Junction", "9008765", "Joe Donnelly");
+        createBook("Finders Keepers", "0087765", "Steven King");
         
-        
-        //create club members
-        Staff member1 = new Staff("Harry Smith", "Male", 27);
-        
+        //create some enthusiastic club members
+        createMember("Christina Sass", "Female", 29);
+        createMember("Harry Smith", "Male", 17);
+        createMember("Jane Doe", "Female", 16);
+        createMember("Dipo Isola", "Male", 18);
+        createMember("Tosin Adesanya", "Male", 27);
+        createMember("Florence Okosun", "Female", 26);
+        createMember("Jeremy Johnson", "Male", 32);
     }
     
     public static boolean createBook(String title, String isbn, String author){
-        return false;
+        Book book = new Book(title, isbn, author);
+        
+        return Objects.nonNull(book);
     }
     
     public static boolean createMember(String name, String sex, int age){
-        return false;
+        Staff member = new Staff(name, sex, age);
+        
+        return Objects.nonNull(member);
     }
     
     public static boolean borrowBook(Members member, Book book){
@@ -35,7 +49,7 @@ public class Club {
     }
     
     /**
-     * utility method to pause the program for specified no of seconds during execution
+     * utility method to pause the program for a specified no of seconds during execution
      * @param secs 
      */
     public static void pauseProg(int secs){
